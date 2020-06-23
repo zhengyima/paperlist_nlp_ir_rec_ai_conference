bf5d238c91df3ad03d5b4bffd9504983612ffb22
lmerengues <317958662@qq.com>
2020-06-23 22:45:31 +0800

    支持search多个关键词功能

---
+++ find.py
@@ -0,0 +1,44 @@
+
+import argparse
+
+page_dict = []
+
+def load_pagedict():
+    f = open("papers_index.txt")
+    for line in f:
+        minrow, maxrow, conference = line.strip().split()
+        page_dict.append([int(minrow), int(maxrow), conference])
+
+def search(keyword):
+    print("query: ", keyword)
+    keys = keyword.strip().split("_")
+    f = open("papers.txt")
+    cnt = 1
+    res_cnt = 0
+    for line in f:
+        line = line.strip().lower()
+        exist = True
+        for k in keys:
+            if k.lower() not in line:
+                exist = False
+                break
+        if exist:
+            for pc in page_dict:
+                if cnt >= pc[0] and cnt <= pc[1]:
+                    print("-------------------------------")
+                    print(pc[2] + ":" + line)
+                    # print("-------------------------------")
+                    res_cnt += 1
+        cnt += 1
+
+    print("have got %d results." % (res_cnt))
+
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='paper search')
+    parser.add_argument('--q',type=str,help='query, delimated by _', default='dialogue generation')
+    args = parser.parse_args()
+
+    load_pagedict()
+
+    search(args.q)
